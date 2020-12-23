de3df8a86a5e6b8bed3c6d57e98b50566ae79a40
MobinArshad <74773257+MobinArshad@users.noreply.github.com>
2020-12-23 20:56:51 +0530

    Hello world java

---
+++ basics/HelloWorld.java
@@ -0,0 +1,12 @@
+//	Hello World in java
+
+public class HelloWorld {
+			// The main method
+			public static void main(String[] args){
+            // Printing hello world in java
+				System.out.println("I am Mobin Arshad Khan");
+		}
+
+
+
+}
